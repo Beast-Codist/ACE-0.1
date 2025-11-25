eb2ca769b253bfeb49ae213c7c465d00da8deca2
Beast-Codist <g.naframitsa@gmail.com>
2025-11-25 20:16:18 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from typing import Union
+from fastapi import FastAPI
+from db import mock_db
+import uvicorn
+import json
+
+
+app = FastAPI()
+
+
+@app.get("/items/{item}", tags = ["Работа с JSON"], summary = "Получить сообщение из базы данных")
+def read_item(item: str, q: Union[str, None] = None):
+    result = mock_db.get(item, "Ключ не найден")
+    return result
+
+
+@app.post("/records", tags = ["Работа с JSON"], summary = "Записать сообщение из JSON в базу данных")
+def create_record():
+    with open("test_data.json", "r", encoding="UTF=8") as rec_file:
+        data = json.load(rec_file)
+        mock_db.update(data)
+        rec_file.close()
+
+    return {"success": "База данных обновлена"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
